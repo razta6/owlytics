c939eedcdc86d5ee66eba597ccfc1453596808d7
razta <razta6@gmail.com>
2020-04-11 14:22:59 +0300

    a

---
+++ deep_ts.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from sklearn.metrics import f1_score
+
+class LSTMClassifier(nn.Module):
+
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+        super().__init__()
+        self.hidden_dim = hidden_dim
+        self.layer_dim = layer_dim
+        self.rnn = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, hidden_dim)
+        self.fc_out = nn.Linear(hidden_dim, output_dim)
+        self.batch_size = None
+        self.hidden = None
+
+    def forward(self, x):
+        h0, c0 = self.init_hidden(x)
+        out, (hn, cn) = self.rnn(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        out = self.fc(out)
+        out = self.fc_out(out)
+        return out
+
+    def init_hidden(self, x):
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim)
+        c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim)
+        return [t.cuda() for t in (h0, c0)]
+
+def Trainer(model, trn_dl, val_dl, n_epochs, sched, opt, criterion, device, patience):
+    print('Start model training')
+    print(model)
+    trials = 0
+    best_acc = 0
+    best_score = 0
+
+    for epoch in range(1, n_epochs + 1):
+
+        for i, (x_batch, y_batch) in enumerate(trn_dl):
+            model.train()
+            x_batch = x_batch.to(device)
+            y_batch = y_batch.to(device)
+            opt.zero_grad()
+            out = model(x_batch)
+            loss = criterion(out, y_batch)
+            loss.backward()
+            opt.step()
+            sched.step()
+
+        model.eval()
+        correct, total = 0, 0
+        total_preds, total_y = [], []
+        for x_val, y_val in val_dl:
+            x_val, y_val = [t.to(device) for t in (x_val, y_val)]
+            out = model(x_val)
+            preds = F.log_softmax(out, dim=1).argmax(dim=1)
+            total += y_val.size(0)
+            correct += (preds == y_val).sum().item()
+
+            total_preds.append(preds)
+            total_y.append(y_val)
+
+        acc = correct / total
+        total_preds = np.concatenate([p.to("cpu") for p in total_preds])
+        total_y = np.concatenate([y.to("cpu") for y in total_y])
+        score = f1_score(total_y, total_preds, average="macro")
+
+        if epoch % 10 == 0:
+            print(f'Epoch: {epoch:3d}. Loss: {loss.item():.4f}. F1 score: {score:2.2%}, Acc.: {acc:2.2%}')
+
+        if score > best_score:
+            trials = 0
+            best_score = score
+            torch.save(model.state_dict(), 'best.pth')
+            print(f'Epoch {epoch} best model saved with f1_score: {best_score:2.2%}')
+        else:
+            trials += 1
+            if trials >= patience:
+                print(f'Early stopping on epoch {epoch}')
+                break
---
+++ utils.py
@@ -0,0 +1,70 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+from torch.optim.lr_scheduler import _LRScheduler
+import matplotlib.pyplot as plt
+
+def create_datasets(X, y, test_size=0.2, time_dim_first=False):
+    enc = LabelEncoder()
+    y_enc = enc.fit_transform(y)
+    X_grouped = create_grouped_array(X)
+    if time_dim_first:
+        X_grouped = X_grouped.transpose(0, 2, 1)
+    X_train, X_valid, y_train, y_valid = train_test_split(X_grouped, y_enc, test_size=test_size)
+    X_train, X_valid = [torch.tensor(arr, dtype=torch.float32) for arr in (X_train, X_valid)]
+    y_train, y_valid = [torch.tensor(arr, dtype=torch.long) for arr in (y_train, y_valid)]
+    train_ds = TensorDataset(X_train, y_train)
+    valid_ds = TensorDataset(X_valid, y_valid)
+    return train_ds, valid_ds, enc
+
+
+def create_grouped_array(data, group_col='series_id'):
+    X_grouped = np.row_stack([
+        group.values[None]
+        for _, group in data.groupby(data.index)])
+    return X_grouped
+
+
+def create_test_dataset(X):
+    X_grouped = np.row_stack([
+        group.values[None]
+        for _, group in X.groupby('series_id')])
+    X_grouped = torch.tensor(X_grouped.transpose(0, 2, 1)).float()
+    y_fake = torch.tensor([0] * len(X_grouped)).long()
+    return TensorDataset(X_grouped, y_fake)
+
+
+def create_loaders(train_ds, valid_ds, bs=512, jobs=0):
+    train_dl = DataLoader(train_ds, bs, shuffle=True, num_workers=jobs)
+    valid_dl = DataLoader(valid_ds, bs, shuffle=False, num_workers=jobs)
+    return train_dl, valid_dl
+
+
+def accuracy(output, target):
+    return (output.argmax(dim=1) == target).float().mean().item()
+
+
+class CyclicLR(_LRScheduler):
+
+    def __init__(self, optimizer, schedule, last_epoch=-1):
+        assert callable(schedule)
+        self.schedule = schedule
+        super().__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        return [self.schedule(self.last_epoch, lr) for lr in self.base_lrs]
+
+
+def cosine(t_max, eta_min=0):
+    def scheduler(epoch, base_lr):
+        t = epoch % t_max
+        return eta_min + (base_lr - eta_min) * (1 + np.cos(np.pi * t / t_max)) / 2
+
+    return scheduler
+
+def plot_cosine_sched(n=100):
+    sched = cosine(n)
+    lrs = [sched(t, 1) for t in range(n * 4)]
+    plt.plot(lrs)
